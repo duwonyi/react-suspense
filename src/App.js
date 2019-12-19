722d1993e92b7d5680e2a113a9e9035ca3324137
Doowon Lee <duwonyi@gmail.com>
2019-12-18 19:49:23 -0500

    UPDATE: dependency and vscode setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Detail = React.lazy(() => import('./detail'))
+const Detail = React.lazy(() => import('./Detail'))
 
 function App() {
   return (
     <React.Suspense fallback={'Loading ...'}>
+      {/* <div>TEST</div> */}
       <Detail />
     </React.Suspense>
   )
